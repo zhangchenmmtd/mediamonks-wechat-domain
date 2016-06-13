a27e3c795567adc266c7482eef77ef54e4f0fb17
neverrinmyself@gmail.com <neverrinmyself@gmail.com>
2016-06-13 15:11:21 +0800

    Since ROS client ask MM to use their sever to fetch wechat jssdkticket, so MM can not call wechat url directly.
    added
    1. few columns in wechataccount domain to let user fill in client server info
    2. more logic when fetch accesstoken and ticket

--- src/main/java/com/mediamonks/domain/WeChatAccount.java
+++ src/main/java/com/mediamonks/domain/WeChatAccount.java
@@ -3,6 +3,7 @@ package com.mediamonks.domain;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 import org.joda.time.Minutes;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.*;
 import java.io.UnsupportedEncodingException;
@@ -23,6 +24,22 @@ public class WeChatAccount extends Domain{
     private String appSecret;
     @Column(name = "ticket")
     private String ticket;
+
+    /**
+     * For some client, they do not want MM to have direct integration with WeChat API since wechat
+     * has some limitation on ticket or accessToken request frequency, so client may build a system as a hub for MM to
+     * obtain the ticket and token, in that case, We need to store both accessToken and ticket from the client side
+     */
+    @Column(name = "accessToken")
+    private String accessToken;
+    @Column(name = "clientserverid")
+    private String clientServerID;
+    @Column(name = "clientserverscecret")
+    private String clientServerSecrect;
+    @Column(name = "accesstokenurl")
+    private String accessTokenUrl;
+    @Column(name = "jssdkticketurl")
+    private String jsSDKTicketUrl;
     @Column(name = "lastupdateat")
     private Date lastUpdatedAt;
     @JoinColumn(name = "clientid")
@@ -73,6 +90,9 @@ public class WeChatAccount extends Domain{
         if(lastUpdatedAt == null || ticket == null){
             return true;
         }
+        if(usingClientServer()){
+            return true;
+        }
 
         LocalDateTime dateTime = new LocalDateTime(lastUpdatedAt);
         return Minutes.minutesBetween(dateTime, new LocalDateTime()).getMinutes() > 60;
@@ -98,5 +118,43 @@ public class WeChatAccount extends Domain{
 
     public void updateStatus() {
         this.enabled = !enabled;
+    }
+
+    public String getAccessTokenUrl() {
+        return accessTokenUrl;
+    }
+
+    public String getJsSDKTicketUrl() {
+        return jsSDKTicketUrl;
+    }
+
+    public void changeUrls(String accessTokenUrl, String jsSDKTicketUrl) {
+        this.accessTokenUrl = accessTokenUrl;
+        this.jsSDKTicketUrl = jsSDKTicketUrl;
+    }
+
+    public void changeAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getClientServerID() {
+        return clientServerID;
+    }
+
+    public String getClientServerSecrect() {
+        return clientServerSecrect;
+    }
+
+    public void changeClientServerInfo(String clientServerID, String clientServerSecrect) {
+        this.clientServerID = clientServerID;
+        this.clientServerSecrect = clientServerSecrect;
+    }
+
+    public boolean usingClientServer() {
+        return StringUtils.hasText(clientServerID);
     }
 }
